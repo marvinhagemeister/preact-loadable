583638d82811f6b7283fa5239940dbef9fab7142
Marvin Hagemeister <marvin@marvinhagemeister.de>
2017-10-31 08:06:14 +0100

    Add loadable component

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,6 +1,60 @@
-describe("foo", () => {
-  it("should return foo", () => {
-    const foo = () => "foo";
-    expect(foo()).toEqual("foo");
+import Loadable, { Props } from "../index";
+
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+const props: Props = {
+  fn: () => Promise.resolve("foo"),
+  loading: () => "loading",
+  error: () => "error",
+};
+
+describe("Loadable", () => {
+  it("should display loading", () => {
+    const loader = new Loadable(props);
+    loader.componentWillMount();
+    expect(loader.render()).toEqual("loading");
+  });
+
+  it("should handle sync", () => {
+    const fn = () => "done";
+    const loader = new Loadable({ ...props, fn });
+    loader.componentWillMount();
+    expect(loader.render()).toEqual("done");
+  });
+
+  it("should handle async", async () => {
+    const fn = () => Promise.resolve().then(() => "done");
+    const loader = new Loadable({ ...props, fn });
+    loader.componentWillMount();
+    await delay(0);
+    expect(loader.render()).toEqual("done");
+  });
+
+  it("should handle async imports", async () => {
+    const fn = () => Promise.resolve().then(() => ({ default: () => "done" }));
+    const loader = new Loadable({ ...props, fn });
+    loader.componentWillMount();
+    await delay(0);
+    expect(loader.render()).toEqual("done");
+  });
+
+  it("should handle error sync", () => {
+    const fn = () => {
+      throw new Error("foo");
+    };
+    const loader = new Loadable({ ...props, fn });
+    loader.componentWillMount();
+    expect(loader.render()).toEqual("error");
+  });
+
+  it("should handle error async", async () => {
+    const fn = () =>
+      Promise.resolve().then(() => {
+        throw new Error("foo");
+      });
+    const loader = new Loadable({ ...props, fn });
+    loader.componentWillMount();
+    await delay(0);
+    expect(loader.render()).toEqual("error");
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,55 @@
-// tslint:disable-next-line
-console.log("hello world");
+import { Component } from "preact";
+
+export interface Props {
+  fn: () => any | Promise<any>;
+  loading: () => any;
+  error: (err: Error) => any;
+}
+
+export interface State {
+  component: any;
+}
+
+export default class Loadable extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { component: undefined };
+  }
+
+  componentWillMount() {
+    const { fn, error } = this.props;
+
+    let p: any;
+    try {
+      p = fn();
+    } catch (err) {
+      return this.setState({ component: () => error(err) });
+    }
+
+    if (p.then !== undefined) {
+      return p
+        .then((c: any) => {
+          this.setState({
+            component: c.default !== undefined ? c.default : () => c,
+          });
+        })
+        .catch((err: Error) => this.setState({ component: () => error(err) }));
+    } else {
+      return this.setState({
+        component: () => (p.default !== undefined ? p.default : p),
+      });
+    }
+  }
+
+  componentWillReceiveProps(props: Props) {
+    this.props = props;
+    // Force render
+    this.componentWillMount();
+  }
+
+  render() {
+    return this.state.component === undefined
+      ? this.props.loading()
+      : this.state.component();
+  }
+}
